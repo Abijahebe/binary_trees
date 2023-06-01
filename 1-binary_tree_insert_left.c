68b6d178015dade14a5102c2e579b78bfb2ef59e
Abijahebe <Abijahebe911@gmail.com>
2023-06-01 06:56:39 -0700

    task 1-2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		node->left = tmp;
 		tmp->parent = node;
 	}
-	parent->left + node;
+	parent->left = node;
 
 	return (node);
 }
